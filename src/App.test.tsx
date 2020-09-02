08de804b1859141f561aa07c4ed3f1c16341502b
yashvekaria <yashvekaria@gmail.com>
2020-09-02 10:46:50 +0530

    Inital Commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,105 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("<App> container tests", () => {
+  beforeEach(() => {
+    const invertName = (App.prototype.invertName = jest.fn());
+  });
+
+  test("renders learn react link", () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("given null returns empty string", () => {
+    assertInverted(null, "");
+  });
+
+  test("given empty string returns empty string", () => {
+    assertInverted("", "");
+  });
+
+  test("given simple name returns simple name", () => {
+    assertInverted("Name", "Name");
+  });
+
+  test("given simple name returns simple name", () => {
+    assertInverted("Name", "Name");
+  });
+
+  test("given simple name with spaces returns simple name without spaces", () => {
+    assertInverted(" Name ", "Name");
+  });
+
+  test("given first last returns last first", () => {
+    assertInverted("First Last", "Last, First");
+  });
+
+  test("given first last with extra spaces returns last first", () => {
+    assertInverted("  First  Last  ", "Last, First");
+  });
+
+  test("ignore Honorific", () => {
+    assertInverted("Mr. First Last", "Last, First");
+    assertInverted("Mrs. First Last", "Last, First");
+  });
+
+  test("post nominals stays at end", () => {
+    assertInverted("First Last Sr.", "Last, First Sr.");
+    assertInverted("First Last BS. Phd.", "Last, First BS. Phd.");
+  });
+
+  test("integration", () => {
+    assertInverted("Mr.  Yash   Vekaria  III  Jr.", "Vekaria, Yash III Jr.");
+  });
+
+  const assertInverted = (originalName: string, invertedName: string) => {
+    expect(invertedName).toBe(invertName(originalName));
+  };
+
+  const invertName = (name: string): string => {
+    if (name == null || name.length <= 0) return "";
+    else return formatName(removeHonorifics(splitNames(name)));
+  };
+
+  const splitNames = (name: string): string[] => {
+    return name.trim().split(/\s+/);
+  };
+
+  const isHonorific = (word: string) => {
+    return word.match(/^(Mr|Mrs)\./);
+  };
+
+  const removeHonorifics = (names: string[]) => {
+    if (names.length > 1 && isHonorific(names[0])) names.splice(0, 1);
+    return names;
+  };
+
+  const formatMultiElementName = (names: string[]) => {
+    let postNominal = getPostNominals(names);
+    const firstName = names[0];
+    const lastName = names[1];
+    return `${lastName}, ${firstName} ${postNominal}`.trim();
+  };
+
+  const formatName = (names: string[]) => {
+    if (names.length === 1) {
+      return names[0];
+    } else {
+      return formatMultiElementName(names);
+    }
+  };
+
+  const getPostNominals = (names: string[]) => {
+    let postNominalString = "";
+    if (names.length > 2) {
+      let postNominals = names.slice(2, names.length);
+      postNominals.forEach((pn: string) => {
+        postNominalString += pn + " ";
+      });
+    }
+    return postNominalString;
+  };
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IProps {}
+
+class App extends React.Component<IProps, {}> {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+
+  invertName = (name: string): string => {
+    if (name == null || name.length <= 0) return "";
+    else return this.formatName(this.removeHonorifics(this.splitNames(name)));
+  };
+
+  splitNames = (name: string): string[] => {
+    return name.trim().split(/\s+/);
+  };
+
+  isHonorific = (word: string) => {
+    return word.match(/^(Mr|Mrs)\./);
+  };
+
+  removeHonorifics = (names: string[]) => {
+    if (names.length > 1 && this.isHonorific(names[0])) names.splice(0, 1);
+    return names;
+  };
+
+  formatMultiElementName = (names: string[]) => {
+    let postNominal = this.getPostNominals(names);
+    const firstName = names[0];
+    const lastName = names[1];
+    return `${lastName}, ${firstName} ${postNominal}`.trim();
+  };
+
+  formatName = (names: string[]) => {
+    if (names.length === 1) {
+      return names[0];
+    } else {
+      return this.formatMultiElementName(names);
+    }
+  };
+
+  getPostNominals = (names: string[]) => {
+    let postNominalString = "";
+    if (names.length > 2) {
+      let postNominals = names.slice(2, names.length);
+      postNominals.forEach((pn: string) => {
+        postNominalString += pn + " ";
+      });
+    }
+    return postNominalString;
+  };
 }
 
 export default App;
